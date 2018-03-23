98de63aa1a9d494e6b526e0b11dbb7f5f7e3d6fe
Barsonax <rick-van-dam@outlook.com>
2018-03-23 21:54:14 +0100

    added some benchmarks

--- Mirror.Benchmarks/Program.cs
+++ Mirror.Benchmarks/Program.cs
@@ -20,45 +20,69 @@ namespace Mirror.Benchmarks
 	[ClrJob]
 	public class ReflectionBenchmark
 	{
-		private Dictionary<Type, Func<object, object>> CachedFuncs = new Dictionary<Type, Func<object, object>>();
-		private Dictionary<Type, MethodInfo> CachedMethodInfo = new Dictionary<Type, MethodInfo>();
+		private Func<object, object> CachedFuncs;
+		private MethodInfo CachedMethodInfo;
 		private Type _type;
+
+		private object _objectInstance;
+		private IReflectionBenchmarkTestClass _interfaceInstance;
+		private ReflectionBenchmarkTestClass _instance;
 
 		public ReflectionBenchmark()
 		{
-			_type = this.GetType();
-			var methodInfo = _type.GetRuntimeMethod(nameof(TestMethod), new Type[0]);
-			CachedMethodInfo.Add(_type, methodInfo);
-			CachedFuncs.Add(_type, DelegateFactory.Create(_type, nameof(TestMethod)));
-		}
-
-
-		[Benchmark]
-		public object MethodInfoInvoke()
-		{
-			var methodInfo = _type.GetRuntimeMethod(nameof(TestMethod), new Type[0]);
-			return methodInfo.Invoke(this, new object[0]);
-		}
-
-		[Benchmark]
-		public object CachedMethodInfoInvoke()
-		{
-			return CachedMethodInfo[_type].Invoke(this, new object[0]);
-		}
-
-		[Benchmark]
-		public object DelegateFactoryInvoke()
-		{
-			return CachedFuncs[_type].Invoke(this);
+			_instance = new ReflectionBenchmarkTestClass();
+			_interfaceInstance = _instance;
+			_objectInstance = _instance;
+			_type = _instance.GetType();
+			CachedMethodInfo = _type.GetRuntimeMethod(nameof(ReflectionBenchmarkTestClass.TestMethod), new Type[0]);
+			CachedFuncs = DelegateFactory.Create(_type, nameof(ReflectionBenchmarkTestClass.TestMethod));
 		}
 
 		[Benchmark]
 		public Func<object, object> CreateDelegate()
 		{
-			return DelegateFactory.Create(_type, nameof(TestMethod));
+			return DelegateFactory.Create(_type, nameof(ReflectionBenchmarkTestClass.TestMethod));
 		}
 
+		[Benchmark]
+		public object MethodInfoInvoke()
+		{
+			var methodInfo = _type.GetRuntimeMethod(nameof(ReflectionBenchmarkTestClass.TestMethod), new Type[0]);
+			return methodInfo.Invoke(_objectInstance, new object[0]);
+		}
 
+		[Benchmark]
+		public object CachedMethodInfoInvoke()
+		{
+			return CachedMethodInfo.Invoke(_objectInstance, new object[0]);
+		}
+
+		[Benchmark]
+		public object DelegateFactoryInvoke()
+		{
+			return CachedFuncs.Invoke(_objectInstance);
+		}
+
+		[Benchmark]
+		public void InterfaceCall()
+		{
+			_interfaceInstance.TestMethod();
+		}
+
+		[Benchmark]
+		public void DirectCall()
+		{
+			_instance.TestMethod();
+		}
+	}
+
+	public interface IReflectionBenchmarkTestClass
+	{
+		void TestMethod();
+	}
+
+	public class ReflectionBenchmarkTestClass : IReflectionBenchmarkTestClass
+	{
 		public void TestMethod()
 		{
 
